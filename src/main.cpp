0c217e16a4a3dae948a27b21b94946fe6805e096
Gin <martin5110719@gmail.com>
2021-07-30 15:33:40 +0800

    3D pyramid

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
     #include <glad/glad.h>
     #include <GLFW/glfw3.h>
     #include <stb/stb_image.h>
+    #include <glm/glm.hpp>
+    #include <glm/gtc/matrix_transform.hpp>
+    #include <glm/gtc/type_ptr.hpp>
     #include <iostream>
     #include <math.h>
     #include "Header/shaderClass.h"
@@ -30,18 +33,22 @@
 
         //Vertices coordinates
         GLfloat vertices[] = 
-        {
-            -0.5f,   -0.5f,  0.0f,   1.0f, 0.0f, 0.0f,  0.0f, 0.0f,// Lower left corner
-            -0.5f,    0.5f,  0.0f,   0.0f, 1.0f, 0.0f,  0.0f, 1.0f,// Lower right corner
-             0.5f,    0.5f,  0.0f,   0.0f, 0.0f, 1.0f,  1.0f, 1.0f,// Upper corner
-             0.5f,   -0.5f,  0.0f,   1.0f, 1.0f, 1.0f,  1.0f, 0.0f//Inner left
+        {//     COORDINATES         /       COLORS        /     TexCoord//
+            -0.5f,    0.0f,  0.5f,   0.83f, 0.70f, 0.44f,  0.0f, 0.0f,// Lower left corner
+            -0.5f,    0.0f, -0.5f,   0.83f, 0.70f, 0.44f,  5.0f, 0.0f,// Lower right corner
+             0.5f,    0.0f, -0.5f,   0.83f, 0.70f, 0.44f,  0.0f, 0.0f,// Upper corner
+             0.5f,    0.0f,  0.5f,   0.83f, 0.70f, 0.44f,  5.0f, 0.0f,//Inner left
+             0.0f,    0.8f,  0.0f,   0.92f, 0.86f, 0.76f,  2.5f, 5.0f
         };
 
         GLuint indices[] = 
         {
-            0, 2, 1, // Lower left triangle
-            0, 3, 2 // LowerRight triangle
-                     // Upper triangle
+            0, 1, 2, // Lower left triangle
+            0, 2, 3, // LowerRight triangle
+            0, 1, 4, // Upper triangle
+            1, 2, 4,
+            2, 3, 4,
+            3, 0, 4
         };
 
         // glfw window creation
@@ -67,7 +74,7 @@
 
         //Specify the viewport of OpenGL in the Window
         //In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-        glViewport(0, 0, 800, 800);
+        glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
         Shader shaderProgram("src/Shaders/VertexShader.vs", "src/Shaders/FragmentShader.fs");
 
@@ -89,7 +96,7 @@
 
         //Texture
 
-        Texture popCat("resource/pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+        Texture popCat("resource/brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
         popCat.texUnit(shaderProgram, "tex0", 0);
 
         // render loop
@@ -98,12 +105,48 @@
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
 
+        //Variables that help the rotation of the pyramid
+        float rotation = 0.0f;
+        double prevTime = glfwGetTime();
+
+        //Enables the Depth Buffer
+        glEnable(GL_DEPTH_TEST);
+
         while (!glfwWindowShouldClose(window))
         {
+            //Specify the color of the background
             glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
+            //Clean the back buffer and depth buffer
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             //Tell OpenGL which Shader Program we want to use
             shaderProgram.Activate();
+
+            //Simple timer
+            double crnTime = glfwGetTime();
+            if(crnTime - prevTime >= 1 / 60)
+            {
+                rotation += 0.5f;
+                prevTime = crnTime;
+            }
+
+            //Initializes matrices so they are not the null matrix
+            glm::mat4 model = glm::mat4(1.0f);
+            glm::mat4 view = glm::mat4(1.0f);
+            glm::mat4 proj = glm::mat4(1.0f);
+
+            //Assigns different transformations to each matrix
+            model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+            view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+            proj = glm::perspective(glm::radians(45.0f), (float)(SCR_WIDTH / SCR_HEIGHT), 0.1f, 100.0f);
+
+            // Outputs the matrices into the Vertex Shader
+            int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+            glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+            int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+            glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
             //Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
             glUniform1f(uniID, 0.5f);
             // Binds texture so that is appears in render
@@ -111,7 +154,7 @@
             //Bind the VAO so OpenGL knows to use it
             VAO1.Bind();
             //Draw the triangle using the GL_TRIANGLES primitive
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
             // glfw: swap buffers and poll IO events (keyspressed/released, mouse moved etc.)
             // ---------------------------------------------------
             glfwSwapBuffers(window);

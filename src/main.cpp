143cf057ada6975ea6ca14da76f6f3d2f2013e6f
Gin <martin5110719@gmail.com>
2021-08-10 17:50:31 +0800

    lighting

--- src/main.cpp
+++ src/main.cpp
@@ -1,154 +1,237 @@
-    #include <glad/glad.h>
-    #include <GLFW/glfw3.h>
-    #include <stb/stb_image.h>
-    #include <glm/glm.hpp>
-    #include <glm/gtc/matrix_transform.hpp>
-    #include <glm/gtc/type_ptr.hpp>
-    #include <iostream>
-    #include <math.h>
-    #include "Header/shaderClass.h"
-    #include "Header/VAO.h"
-    #include "Header/VBO.h"
-    #include "Header/EBO.h"
-    #include "Header/Texture.h"
-    #include "Header/camera.h"
-    // settings
-    const unsigned int SCR_WIDTH = 800;
-    const unsigned int SCR_HEIGHT = 800;
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <math.h>
+#include "Header/shaderClass.h"
+#include "Header/VAO.h"
+#include "Header/VBO.h"
+#include "Header/EBO.h"
+#include "Header/Texture.h"
+#include "Header/camera.h"
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;
 
-    //Vertex Shader source code.
-   
-    //Fragment Shader source code
-    
-    int main()
+//Vertex Shader source code.
+
+//Fragment Shader source code
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //uncomment this statement to fix compilation on OS X
+#endif
+
+    //Vertices coordinates
+    GLfloat vertices[] =
+    {  //     COORDINATES     /        COLORS          /    TexCoord   /        NORMALS       //
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+        0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+
+        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+        0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.8f, 0.5f,  0.0f, // Right side
+
+        0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.0f, 0.5f,  0.8f  // Facing side
+    };
+
+    GLuint indices[] =
+        {
+            0, 1, 2, // Bottom side
+            0, 2, 3, // Bottom side
+            4, 6, 5, // Left side
+            7, 9, 8, // Non-facing side
+            10, 12, 11, // Right side
+            13, 15, 14 // facing side
+        };
+
+    GLfloat lightVertices[] =
+        {//  COORDINATES    //
+         -0.1f, -0.1f, 0.1f,
+         -0.1f, -0.1f, -0.1f,
+         0.1f, -0.1f, -0.1f,
+         0.1f, -0.1f, 0.1f,
+         -0.1f, 0.1f, 0.1f,
+         -0.1f, 0.1f, -0.1f,
+         0.1f, 0.1f, -0.1f,
+         0.1f, 0.1f, 0.1f};
+
+    GLuint lightIndices[] = 
     {
-        // glfw: initialize and configure
-        // ------------------------------
-        glfwInit();
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    #ifdef __APPLE__
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //uncomment this statement to fix compilation on OS X
-    #endif
+        0, 1, 2,
+        0, 2, 3,
+        0, 4, 7,
+        0, 7, 3,
+        3, 7, 6,
+        3, 6, 2,
+        2, 6, 5,
+        2, 5, 1,
+        1, 5, 4,
+        1, 4, 0,
+        4, 5, 6,
+        4, 6, 7
+    };
 
-        //Vertices coordinates
-        GLfloat vertices[] = 
-        {//     COORDINATES         /       COLORS        /     TexCoord//
-            -0.5f,    0.0f,  0.5f,   0.83f, 0.70f, 0.44f,  0.0f, 0.0f,// Lower left corner
-            -0.5f,    0.0f, -0.5f,   0.83f, 0.70f, 0.44f,  5.0f, 0.0f,// Lower right corner
-             0.5f,    0.0f, -0.5f,   0.83f, 0.70f, 0.44f,  0.0f, 0.0f,// Upper corner
-             0.5f,    0.0f,  0.5f,   0.83f, 0.70f, 0.44f,  5.0f, 0.0f,//Inner left
-             0.0f,    0.8f,  0.0f,   0.92f, 0.86f, 0.76f,  2.5f, 5.0f
-        };
+    // glfw window creation
+    // --------------------
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-        GLuint indices[] = 
-        {
-            0, 1, 2, // Lower left triangle
-            0, 2, 3, // LowerRight triangle
-            0, 1, 4, // Upper triangle
-            1, 2, 4,
-            2, 3, 4,
-            3, 0, 4
-        };
+    //Load GLAD so it configures OpenGL
+    gladLoadGL();
 
-        // glfw window creation
-        // --------------------
-        GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,"OpenGL", NULL, NULL);
-        if (window == NULL)
-        {
-            std::cout << "Failed to create GLFW window" << std::endl;
-            glfwTerminate();
-            return -1;
-        }
-        glfwMakeContextCurrent(window);
-        // glad: load all OpenGL function pointers
-        // ---------------------------------------
-        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-        {
-            std::cout << "Failed to initialize GLAD" << std::endl;
-            return -1;
-        }
+    //Specify the viewport of OpenGL in the Window
+    //In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
-        //Load GLAD so it configures OpenGL
-        gladLoadGL();
+    Shader shaderProgram("src/Shaders/VertexShader.vs", "src/Shaders/FragmentShader.fs");
 
-        //Specify the viewport of OpenGL in the Window
-        //In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-        glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    VAO VAO1;
+    VAO1.Bind();
 
-        Shader shaderProgram("src/Shaders/VertexShader.vs", "src/Shaders/FragmentShader.fs");
+    VBO VBO1(vertices, sizeof(vertices));
+    EBO EBO1(indices, sizeof(indices));
 
-        VAO VAO1;
-        VAO1.Bind();
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 11 * sizeof(float), (void *)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 11 * sizeof(float), (void *)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 11 * sizeof(float), (void *)(6 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 3, 3, GL_FLOAT, 11 * sizeof(float), (void *)(8 * sizeof(float)));
+    VAO1.Unbind();
+    VBO1.Unbind();
+    EBO1.Unbind();
 
-        VBO VBO1(vertices, sizeof(vertices));
-        EBO EBO1(indices, sizeof(indices));
+    // Shader for light cube
+    Shader lightShader("src/Shaders/Light.vs", "src/Shaders/Light.fs");
+    // Generates Vertex Array Object and binds it
+    VAO lightVAO;
+    lightVAO.Bind();
+    // Generates Vertex Buffer Object and links it to vertices
+    VBO lightVBO(lightVertices, sizeof(lightVertices));
+    //Generates Elemnet Buffer Object and links it io indices
+    EBO lightEBO(lightIndices, sizeof(lightIndices));
+    // Links VBO attributes such as coordinates and colors to VAO
+    lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
+    // Unbind all to prevent accidentally modify them
+    lightVAO.Unbind();
+    lightVBO.Unbind();
+    lightEBO.Unbind();
 
-        VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-        VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-        VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-        VAO1.Unbind();
-        VBO1.Unbind();
-        EBO1.Unbind();
+    glm::vec4 lightColor = glm::vec4(0.8f, 0.5f, 0.3f, 1.0f);
 
+    glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+    glm::mat4 lightModel = glm::mat4(1.0f);
+    lightModel = glm::translate(lightModel, lightPos);
+
+    glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::mat4 pyramidModel = glm::mat4(1.0f);
+    pyramidModel = glm::translate(pyramidModel, pyramidPos);
+
+    lightShader.Activate();
+    glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+    glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+    shaderProgram.Activate();
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+    glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+    glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightColor.x, lightColor.y, lightColor.z);
+
+    //Texture
+
+    Texture popCat("resource/pyramid.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+    popCat.texUnit(shaderProgram, "tex0", 0);
+
+    // render loop
+    // -----------
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+
+    //Enables the Depth Buffer
+    glEnable(GL_DEPTH_TEST);
+
+    //Creates camera onject
+    Camera camera(SCR_WIDTH, SCR_HEIGHT, glm::vec3(0.0f, 0.0f, 2.0f));
+
+    while (!glfwWindowShouldClose(window))
+    {
+        //Specify the color of the background
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        //Clean the back buffer and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
        
 
-        //Texture
+        //Handles camera inputs
+        camera.Inputs(window);
+        //Updates and exports the camera matrix to the Vertex Shader
+        camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
-        Texture popCat("resource/pyramid.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-        popCat.texUnit(shaderProgram, "tex0", 0);
+         //Tell OpenGL which Shader Program we want to use
+        shaderProgram.Activate();
+        // Exports the camera Position to the Fragment Shader for specular lighting
+        glUniform3f(glGetUniformLocation(shaderProgram.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
+        // Exports the camMatrix to the Vertex Shader of the pyramid
+        camera.Matrix(shaderProgram, "camMatrix");
 
-        // render loop
-        // -----------
-        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        // Binds texture so that is appears in render
+        popCat.Bind();
+        //Bind the VAO so OpenGL knows to use it
+        VAO1.Bind();
+        //Draw the triangle using the GL_TRIANGLES primitive
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
+        lightShader.Activate();
+        camera.Matrix(lightShader, "camMatrix");
+        lightVAO.Bind();
+        glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
+        // glfw: swap buffers and poll IO events (keyspressed/released, mouse moved etc.)
+        // ---------------------------------------------------
         glfwSwapBuffers(window);
-
-        
-
-        //Enables the Depth Buffer
-        glEnable(GL_DEPTH_TEST);
-
-        //Creates camera onject
-        Camera camera(SCR_WIDTH, SCR_HEIGHT, glm::vec3(0.0f, 0.0f, 2.0f));
-
-        while (!glfwWindowShouldClose(window))
-        {
-            //Specify the color of the background
-            glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-            //Clean the back buffer and depth buffer
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            //Tell OpenGL which Shader Program we want to use
-            shaderProgram.Activate();
-
-            //Handles camera inputs
-            camera.Inputs(window);
-            //Updates and exports the camera matrix to the Vertex Shader
-            camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
-
-           
-            // Binds texture so that is appears in render
-            popCat.Bind();
-            //Bind the VAO so OpenGL knows to use it
-            VAO1.Bind();
-            //Draw the triangle using the GL_TRIANGLES primitive
-            glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
-            // glfw: swap buffers and poll IO events (keyspressed/released, mouse moved etc.)
-            // ---------------------------------------------------
-            glfwSwapBuffers(window);
-            glfwPollEvents();
-        }
-
-        //Delete all the object we've created
-       VAO1.Delete();
-       VBO1.Delete();
-       EBO1.Delete();
-       popCat.Delete();
-       shaderProgram.Delete();
-
-        // glfw: terminate, clearing all previously allocated GLFWresources.
-        //---------------------------------------------------------------
-        glfwTerminate();
-        return 0;
+        glfwPollEvents();
     }
+
+    //Delete all the object we've created
+    VAO1.Delete();
+    VBO1.Delete();
+    EBO1.Delete();
+    popCat.Delete();
+    shaderProgram.Delete();
+
+    // glfw: terminate, clearing all previously allocated GLFWresources.
+    //---------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
